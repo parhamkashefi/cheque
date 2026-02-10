bd90993e2c7c2467515c362e01f48ac327e29dd0
parhamkashefi <prhmkashefi@gmail.com>
2026-02-10 20:25:20 +0330

    Refactor code structure for improved readability and maintainability

--- dockerfile
+++ dockerfile
@@ -1,28 +1,20 @@
 FROM node:20-alpine
 
-RUN apk add --no-cache \
-    chromium nss freetype harfbuzz ca-certificates ttf-freefont nodejs
-
-ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser \
-    PUPPETEER_SKIP_DOWNLOAD=true
-
 WORKDIR /usr/src/app
 
-COPY package*.json ./
-RUN apk add --no-cache curl
-RUN npm install --legacy-peer-deps
+RUN npm config set registry https://mirror-npm.runflare.com
+
+RUN corepack enable
+
+
+COPY package.json yarn.lock ./
+
+RUN yarn install
 
 COPY . .
-
-HEALTHCHECK --interval=30s --timeout=3s --start-period=5s --retries=3 \
-  CMD node healthcheck.js
-  
-RUN addgroup -g 1001 -S nodejs
-RUN adduser -S nestjs -u 1001
-RUN chown -R nestjs:nodejs /usr/src/app
-USER nestjs
 
 RUN npm run build
 
 EXPOSE 3000
-CMD ["npm", "run", "start:dev"]
+
+CMD ["yarn", "start:dev"]
--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for Swagger UI
-  app.enableCors();
-
   const config = new DocumentBuilder()
     .setTitle('Cheque APIS')
-    .setDescription('API for create and manage cheques')
+    .setDescription('API for managing cheque')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customSiteTitle: 'Cheque API Documentation',
-    customCss: '.swagger-ui .topbar { display: none }',
-    customfavIcon: '/favicon.ico',
-  });
+  SwaggerModule.setup('api', app, document);
 
-  // Bind to 0.0.0.0 to make it accessible from outside the container
-  await app.listen(3000, '0.0.0.0');
-  console.log(`Application is running on: http://0.0.0.0:3000`);
-  console.log(`Swagger documentation: http://0.0.0.0:3000/api`);
+  await app.listen(3000);
 }
 bootstrap();
